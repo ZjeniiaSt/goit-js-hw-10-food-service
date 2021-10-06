c10090b627fa91b6e7d66dfff889c6ea226e85f4
ZjeniiaSt <zeniiastupjak@gmail.com>
2021-10-06 11:24:57 +0300

    themes

--- src/js/themes.js
+++ src/js/themes.js
@@ -4,17 +4,28 @@ const Theme = {
 };
 
 const checkboxRef = document.querySelector('#theme-switch-toggle');
-const bodeRef = document.body;
+const bodyRef = document.body;
 
 checkboxRef.addEventListener('change', onChangeTheme);
 
-function onChangeTheme(event) {
-  event.currentTarget.checked
-    ? toggleTheme(Theme.DARK, Theme.LIGHT)
-    : toggleTheme(Theme.LIGHT, Theme.DARK);
+function onChangeTheme() {
+  if (checkboxRef.checked) {
+    toggleTheme(Theme.DARK, Theme.LIGHT);
+    localStorage.setItem('theme', Theme.DARK);
+  } else {
+    toggleTheme(Theme.LIGHT, Theme.DARK);
+    localStorage.removeItem('theme');
+    localStorage.setItem('theme', Theme.LIGHT);
+  }
 }
 
 function toggleTheme(add, remove) {
-  bodeRef.classList.add(add);
-  bodeRef.classList.remove(remove);
+  bodyRef.classList.add(add);
+  bodyRef.classList.remove(remove);
+}
+
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme === Theme.DARK) {
+  checkboxRef.checked = true;
+  toggleTheme(Theme.DARK, Theme.LIGHT);
 }
